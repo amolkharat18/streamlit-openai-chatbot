2d96fa09f3b8c64d66864eb4888f4d6b006cb8e4
Amol Kharat <amolkharat@yahoo.com>
2024-11-18 09:23:07 +0000

    Final commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+load_dotenv()
+
+st.title("🤖 My Chatbot App")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
